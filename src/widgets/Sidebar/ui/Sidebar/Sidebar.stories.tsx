d09c8785e54da7ccfc084d9944cc65d4a250dacf
Vladislav <faustus15@mail.ru>
2023-10-05 21:21:43 +0300

    fix storybook

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator';
 import { THEMES } from '@/app/providers/ThemeProvider/lib/ThemesContext';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -14,7 +15,22 @@ const Template: ComponentStory<typeof Sidebar> = (args) => (
   />
 );
 
+const authStoreDecorator = StoreDecorator({
+  user: { authData: {} },
+});
+
 export const Light = Template.bind({});
+Light.decorators = [authStoreDecorator];
 
 export const Dark = Template.bind({});
-Dark.decorators = [ThemeDecorator(THEMES.DARK)];
+Dark.decorators = [
+  ThemeDecorator(THEMES.DARK),
+  authStoreDecorator,
+];
+
+export const NoAuth = Template.bind({});
+NoAuth.decorators = [
+  StoreDecorator({
+    user: { authData: undefined },
+  }),
+];
