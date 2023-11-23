f4e95f6092114d6fb91876c4a58541191553b73c
Vladislav <faustus15@mail.ru>
2023-11-23 18:07:34 +0300

    try to fix import error 3

--- src/entities/User/model/selectors/GetUserAuthData/getUserAuthData.ts
+++ src/entities/User/model/selectors/GetUserAuthData/getUserAuthData.ts
@@ -1,5 +1,3 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-const getUserAuthData = (state: StateSchema) => state.user.authData;
-
-export { getUserAuthData };
+export const getUserAuthData = (state: StateSchema) => state.user.authData;
