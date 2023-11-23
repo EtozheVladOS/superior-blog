5ad4b6f2ad7c1e6fbae2efeb522b4d584428ca55
Vladislav <faustus15@mail.ru>
2023-11-23 18:45:27 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,19 @@ import { LOCALSTORAGE_USER_KEY } from '@/shared/constants/localStorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(LOCALSTORAGE_USER_KEY) || '',
-  },
+  // headers: {
+  //   authorization: localStorage.getItem(LOCALSTORAGE_USER_KEY) || '',
+  // },
+});
+
+// interceptors.request отрабатывает на каждый запрос
+$api.interceptors.request.use((config) => {
+  // Для того чтобы при первом заходе работало корреткно, в ином случае апишка
+  // не переинициализирует хеддерсы с инфой по юзеру
+  // (выше закомменчен старый вариант)
+  if (config.headers !== undefined) {
+    config.headers.Authorization = localStorage.getItem(LOCALSTORAGE_USER_KEY) || '';
+  }
+
+  return config;
 });
